79e450be4d18eb37c6338ef81c3603c2f366d928
PNirmal <NirmalQA.Automation@gmail.com>
2018-06-15 13:07:10 +0530

    Interview Task - Medhok

--- src/main/java/medhok/util/TestUtil.java
+++ src/main/java/medhok/util/TestUtil.java
@@ -3,7 +3,7 @@ package medhok.util;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.io.FileUtils;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -14,15 +14,6 @@ public class TestUtil extends TestBase {
 	public static long IMPLITITLY_WAIT=30;
 	public static long PAGE_LOAD_TIMEOUT=30;
 	public static long SET_SCRIPT_TIMEOUT=30;
-	
-	public static String takeScreenshot(long l) throws IOException {
-		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		String destFile = System.getProperty("user.dir")+ "/errorScreenNG/" + l + System.currentTimeMillis()+ ".png";
-		File destination=new File(destFile);
-		FileUtils.copyFile(scrFile, destination);
-		return destFile;
 		
-		}
-	
 
 }
